47d7a71667ecb7e8aa0f33367ebc6eb7bcfc4870
Matt Chapman <chapabu101@gmail.com>
2019-11-11 02:06:37 +0000

    Add Card ad Avatar components

---
+++ src/styleguide/components/00-foundation/15-image/image.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  context: {
+    src: 'https://dummyimage.com/600x400/000/00b6df',
+    srcset: [
+      'https://dummyimage.com/300x200/000/00b6df 300w',
+      'https://dummyimage.com/600x400/000/00b6df 600w',
+      'https://dummyimage.com/1200x800/000/00b6df 1200w',
+      'https://dummyimage.com/2400x1600/000/00b6df 2400w'
+    ],
+    sizes: [],
+    alt: '',
+    classes: []
+  }
+};
---
+++ src/styleguide/components/05-blocks/10-avatar/avatar.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  context: {
+    src: 'https://dummyimage.com/278x278/000/00b6df',
+    alt: '',
+    srcset: [
+      'https://dummyimage.com/278/000/00b6df 300w',
+      'https://dummyimage.com/556/000/00b6df 600w'
+    ]
+  }
+};
